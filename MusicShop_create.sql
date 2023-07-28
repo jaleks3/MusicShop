28c2ab5b13d92847549eca529f5a33356a3ab068
jaleks3 <s24592@pjwstk.edu.pl>
2023-07-28 17:27:24 +0200

    Added sql scripts

---
+++ MusicShop_create.sql
@@ -0,0 +1,199 @@
+-- Created by Vertabelo (http://vertabelo.com)
+-- Last modification date: 2023-07-28 15:06:32.331
+
+-- tables
+-- Table: Address
+CREATE TABLE Address (
+    id int  NOT NULL,
+    city text  NOT NULL,
+    country text  NOT NULL,
+    state text  NOT NULL,
+    postcode int  NOT NULL,
+    street_name text  NOT NULL,
+    street_number int  NOT NULL,
+    CONSTRAINT Address_pk PRIMARY KEY  (id)
+);
+
+-- Table: Author
+CREATE TABLE Author (
+    id int  NOT NULL,
+    name text  NOT NULL,
+    CONSTRAINT Author_pk PRIMARY KEY  (id)
+);
+
+-- Table: Customer
+CREATE TABLE Customer (
+    id int  NOT NULL,
+    name text  NOT NULL,
+    surname text  NOT NULL,
+    Discount_id int  NULL,
+    Address_id int  NOT NULL,
+    CONSTRAINT Customer_pk PRIMARY KEY  (id)
+);
+
+-- Table: Discount
+CREATE TABLE Discount (
+    id int  NOT NULL,
+    Percentage int  NOT NULL,
+    MaxDiscount int  NOT NULL,
+    MinPrice int  NOT NULL,
+    CONSTRAINT Discount_pk PRIMARY KEY  (id)
+);
+
+-- Table: Distributor
+CREATE TABLE Distributor (
+    id int  NOT NULL,
+    name text  NOT NULL,
+    CONSTRAINT Distributor_pk PRIMARY KEY  (id)
+);
+
+-- Table: Genre
+CREATE TABLE Genre (
+    id int  NOT NULL,
+    name text  NOT NULL,
+    description text  NOT NULL,
+    CONSTRAINT Genre_pk PRIMARY KEY  (id)
+);
+
+-- Table: Order
+CREATE TABLE "Order" (
+    id int  NOT NULL,
+    quantity int  NOT NULL,
+    Address_id int  NOT NULL,
+    Status_id int  NOT NULL,
+    RequestAt datetime  NOT NULL,
+    FulfillAt datetime  NOT NULL,
+    CONSTRAINT Order_pk PRIMARY KEY  (id)
+);
+
+-- Table: Order_Record
+CREATE TABLE Order_Record (
+    Record_id int  NOT NULL,
+    Order_id int  NOT NULL,
+    Quantity int  NOT NULL,
+    CONSTRAINT Order_Record_pk PRIMARY KEY  (Record_id,Order_id)
+);
+
+-- Table: Record
+CREATE TABLE Record (
+    id int  NOT NULL,
+    name text  NOT NULL,
+    price int  NOT NULL,
+    description text  NOT NULL,
+    released date  NOT NULL,
+    Author_id int  NOT NULL,
+    Distributor_id int  NOT NULL,
+    TypeOfRecord_id int  NOT NULL,
+    CONSTRAINT Record_pk PRIMARY KEY  (id)
+);
+
+-- Table: Record_Genre
+CREATE TABLE Record_Genre (
+    Record_id int  NOT NULL,
+    Genre_id int  NOT NULL,
+    CONSTRAINT Record_Genre_pk PRIMARY KEY  (Record_id,Genre_id)
+);
+
+-- Table: Record_Storage
+CREATE TABLE Record_Storage (
+    Record_id int  NOT NULL,
+    Storage_id int  NOT NULL,
+    Quantity int  NOT NULL,
+    CONSTRAINT Record_Storage_pk PRIMARY KEY  (Record_id,Storage_id)
+);
+
+-- Table: Status
+CREATE TABLE Status (
+    id int  NOT NULL,
+    name text  NOT NULL,
+    CONSTRAINT Status_pk PRIMARY KEY  (id)
+);
+
+-- Table: Storage
+CREATE TABLE Storage (
+    id int  NOT NULL,
+    Address_id int  NOT NULL,
+    CONSTRAINT Storage_pk PRIMARY KEY  (id)
+);
+
+-- Table: Type_Of_Record
+CREATE TABLE Type_Of_Record (
+    id int  NOT NULL,
+    name text  NOT NULL,
+    color text  NULL,
+    CONSTRAINT Type_Of_Record_pk PRIMARY KEY  (id)
+);
+
+-- foreign keys
+-- Reference: Customer_Address (table: Customer)
+ALTER TABLE Customer ADD CONSTRAINT Customer_Address
+    FOREIGN KEY (Address_id)
+    REFERENCES Address (id);
+
+-- Reference: Customer_Discount (table: Customer)
+ALTER TABLE Customer ADD CONSTRAINT Customer_Discount
+    FOREIGN KEY (Discount_id)
+    REFERENCES Discount (id);
+
+-- Reference: Order_Address (table: Order)
+ALTER TABLE "Order" ADD CONSTRAINT Order_Address
+    FOREIGN KEY (Address_id)
+    REFERENCES Address (id);
+
+-- Reference: Order_Record_Order (table: Order_Record)
+ALTER TABLE Order_Record ADD CONSTRAINT Order_Record_Order
+    FOREIGN KEY (Order_id)
+    REFERENCES "Order" (id);
+
+-- Reference: Order_Record_Record (table: Order_Record)
+ALTER TABLE Order_Record ADD CONSTRAINT Order_Record_Record
+    FOREIGN KEY (Record_id)
+    REFERENCES Record (id);
+
+-- Reference: Order_Status (table: Order)
+ALTER TABLE "Order" ADD CONSTRAINT Order_Status
+    FOREIGN KEY (Status_id)
+    REFERENCES Status (id);
+
+-- Reference: RecordStorage_Record (table: Record_Storage)
+ALTER TABLE Record_Storage ADD CONSTRAINT RecordStorage_Record
+    FOREIGN KEY (Record_id)
+    REFERENCES Record (id);
+
+-- Reference: RecordStorage_Storage (table: Record_Storage)
+ALTER TABLE Record_Storage ADD CONSTRAINT RecordStorage_Storage
+    FOREIGN KEY (Storage_id)
+    REFERENCES Storage (id);
+
+-- Reference: Record_Author (table: Record)
+ALTER TABLE Record ADD CONSTRAINT Record_Author
+    FOREIGN KEY (Author_id)
+    REFERENCES Author (id);
+
+-- Reference: Record_Distributor (table: Record)
+ALTER TABLE Record ADD CONSTRAINT Record_Distributor
+    FOREIGN KEY (Distributor_id)
+    REFERENCES Distributor (id);
+
+-- Reference: Record_Genre_Genre (table: Record_Genre)
+ALTER TABLE Record_Genre ADD CONSTRAINT Record_Genre_Genre
+    FOREIGN KEY (Genre_id)
+    REFERENCES Genre (id);
+
+-- Reference: Record_Genre_Record (table: Record_Genre)
+ALTER TABLE Record_Genre ADD CONSTRAINT Record_Genre_Record
+    FOREIGN KEY (Record_id)
+    REFERENCES Record (id);
+
+-- Reference: Record_TypeOfRecord (table: Record)
+ALTER TABLE Record ADD CONSTRAINT Record_TypeOfRecord
+    FOREIGN KEY (TypeOfRecord_id)
+    REFERENCES Type_Of_Record (id);
+
+-- Reference: Storage_Address (table: Storage)
+ALTER TABLE Storage ADD CONSTRAINT Storage_Address
+    FOREIGN KEY (Address_id)
+    REFERENCES Address (id);
+
+-- End of file.
+
---
+++ MusicShop_seed.sql
@@ -0,0 +1,103 @@
+-- Tabela: Address
+INSERT INTO Address (id, city, country, state, postcode, street_name, street_number)
+VALUES
+    (1, 'New York', 'USA', 'New York', 12345, 'Broadway', 10),
+    (2, 'Los Angeles', 'USA', 'California', 54321, 'Sunset Blvd', 20),
+    (3, 'London', 'UK', 'England', 123456, 'Buckingham Palace Road', 1),
+    (4, 'Tokyo', 'Japan', 'Tokyo', 1000002, 'Chiyoda', 3),
+    (5, 'Berlin', 'Germany', 'Berlin', 10117, 'Brandenburg Gate', 10),
+    (6, 'Paris', 'France', 'Île-de-France', 75000, 'Champs-Élysées', 25),
+    (7, 'Sydney', 'Australia', 'New South Wales', 2000, 'Sydney Opera House', 2);
+
+-- Tabela: Author
+INSERT INTO Author (id, name)
+VALUES
+    (1, 'John Smith'),
+    (2, 'Emma Johnson'),
+    (3, 'Michael Williams');
+
+-- Tabela: Customer
+INSERT INTO Customer (id, name, surname, Discount_id, Address_id)
+VALUES
+    (1, 'David', 'Miller', 1, 3),
+    (2, 'Emily', 'Anderson', 2, 5),
+    (3, 'Daniel', 'White', 3, 1);
+
+-- Tabela: Discount
+INSERT INTO Discount (id, Percentage, MaxDiscount, MinPrice)
+VALUES
+    (1, 10, 50, 100),
+    (2, 20, 75, 150),
+    (3, 15, 60, 120);
+
+-- Tabela: Distributor
+INSERT INTO Distributor (id, name)
+VALUES
+    (1, 'MusicMaster Distribution'),
+    (2, 'Harmony Records'),
+    (3, 'Global Soundwaves'),
+    (4, 'Melody Lane Distributors'),
+    (5, 'BeatStreet Music');
+
+-- Tabela: Genre
+INSERT INTO Genre (id, name, description)
+VALUES
+    (1, 'Rock', 'Rock music is a broad genre of popular music that originated as "rock and roll" in the United States in the late 1940s and early 1950s.'),
+    (2, 'Pop', 'Pop music is a genre of popular music that originated in its modern form during the mid-1950s in the United States and the United Kingdom.'),
+    (3, 'Hip Hop', 'Hip hop music, also known as rap music, is a genre of popular music developed in the United States by inner-city African Americans and Latino Americans.');
+
+-- Tabela: "Order"
+INSERT INTO "Order" (id, quantity, Address_id, Status_id, RequestAt, FulfillAt)
+VALUES
+    (1, 5, 1, 1, '2023-07-28 10:00:00', '2023-07-28 15:00:00'),
+    (2, 3, 2, 2, '2023-07-28 11:30:00', '2023-07-28 16:45:00'),
+    (3, 7, 5, 3, '2023-07-28 09:15:00', '2023-07-28 14:30:00');
+
+-- Tabela: Order_Record
+INSERT INTO Order_Record (Record_id, Order_id, Quantity)
+VALUES
+    (1, 1, 2),
+    (2, 1, 3),
+    (3, 2, 1);
+
+-- Tabela: Record
+INSERT INTO Record (id, name, price, description, released, Author_id, Distributor_id, TypeOfRecord_id)
+VALUES
+    (1, 'Thriller', 30, 'Thriller is the sixth studio album by American singer Michael Jackson, released on November 30, 1982.', '1982-11-30', 1, 1, 1),
+    (2, 'The Dark Side of the Moon', 25, 'The Dark Side of the Moon is the eighth studio album by the English progressive rock band Pink Floyd, released on March 1, 1973.', '1973-03-01', 2, 2, 2),
+    (3, 'Rumours', 20, 'Rumours is the eleventh studio album by British-American rock band Fleetwood Mac, released on February 4, 1977.', '1977-02-04', 3, 3, 1);
+
+-- Tabela: Record_Genre
+INSERT INTO Record_Genre (Record_id, Genre_id)
+VALUES
+    (1, 2),
+    (2, 1),
+    (3, 1);
+
+-- Tabela: Record_Storage
+INSERT INTO Record_Storage (Record_id, Storage_id, Quantity)
+VALUES
+    (1, 1, 10),
+    (2, 2, 5),
+    (3, 3, 15);
+
+-- Tabela: Status
+INSERT INTO Status (id, name)
+VALUES
+    (1, 'Order Received'),
+    (2, 'Processing'),
+    (3, 'Fulfilled');
+
+-- Tabela: Storage
+INSERT INTO Storage (id, Address_id)
+VALUES
+    (1, 2),
+    (2, 4),
+    (3, 6);
+
+-- Tabela: Type_Of_Record
+INSERT INTO Type_Of_Record (id, name, color)
+VALUES
+    (1, 'Vinyl', 'Black'),
+    (2, 'Tape', NULL),
+    (3, 'CD', NULL);
