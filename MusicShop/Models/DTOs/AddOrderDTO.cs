59662e9936f797270fa0e0ef3488157064f07953
jaleks3 <s24592@pjwstk.edu.pl>
2024-02-12 13:54:31 +0100

    Created AddOrderDTO

---
+++ MusicShop/Models/DTOs/AddOrderDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace MusicShop.Models.DTOs
+{
+    public class AddOrderDTO
+    {
+        public int Quantity { get; set; }
+        public DateTime RequestAt { get; set; }
+
+        public DateTime FulfillAt { get; set; }
+        public virtual Address Address { get; set; } = null!;
+        public IEnumerable<int> RecordsId { get; set; } = new List<int>();
+        public virtual Status Status { get; set; } = null!;
+    }
+}
